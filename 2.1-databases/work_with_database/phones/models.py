66e1e52d03afe6b9652080a5aa490f17e5df9344
Александр Лацинов <a8880@mail.ru>
2022-11-07 21:53:13 +0300

    Task 2.1 part 1 complete

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+
+    name = models.CharField(max_length=50, null=False)
+    price = models.PositiveIntegerField()
+    image = models.URLField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField(default=True)
+    slug = models.SlugField(max_length=100, unique=True)
